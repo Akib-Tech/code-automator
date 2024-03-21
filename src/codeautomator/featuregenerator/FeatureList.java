18b2a88f932aa6b3402b18e87510b50dce0b2639
unknown <Ibraheemakin201@gmail.com>
2024-03-21 01:22:42 +0100

    Restricting the type of class to use as value for hashmap

--- src/codeautomator/featuregenerator/FeatureList.java
+++ src/codeautomator/featuregenerator/FeatureList.java
@@ -2,7 +2,7 @@ package codeautomator.featuregenerator;
 import java.util.HashMap;
 import java.util.Map;
 public class FeatureList{
-   static Map <String, Class<?> > featureListMap;
+   static Map <String, Class<Feature> > featureListMap;
    static{
     featureListMap = new HashMap<>();
     featureListMap.put("header", HeaderFeatures.class);
